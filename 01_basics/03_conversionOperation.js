0a3a2252f634d5ebf1219f52ddbd66897f363221
shahzainbhatti190 <shahzainbhatti190@gmail.com>
2026-02-17 11:46:14 +0000

    operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,78 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// convert to Number 
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+let IsLoggedIn = ""
+
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+// convert to Boolean
+
+// 1 => true
+// 0 => false
+// "shahzain" => true
+// "" => false
+
+
+let SomeNumber = 33
+
+let StringNumber = String(SomeNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+
+
+// *************************************** Operations***********************************
+
+let value = 3
+
+let NegValue = -value
+
+// console.log(NegValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hellow"
+let str2 = " shahzain"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 2 * 5 % 3);    //not a good code practice use parentheses 
+
+
+// console.log(+true);
+// console.log(-"");
+
+let gameCounter = 100
+
+//  ++gameCounter;     // prefix increment operator
+gameCounter++;      // postfix increment operator
+
+console.log(gameCounter);
+
+
