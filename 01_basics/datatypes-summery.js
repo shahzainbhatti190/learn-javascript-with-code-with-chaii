7e7dc631aa39a04c066ff5a71ce9951810185016
shahzainbhatti190 <shahzainbhatti190@gmail.com>
2026-02-18 16:29:52 +0000

    datatype-summery

--- 01_basics/datatypes-summery.js
+++ 01_basics/datatypes-summery.js
@@ -11,3 +11,23 @@
 // 5 :  Undefinied
 // 6 :  Symbol
 // 7 :  BigInt 
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const BigInt = 123345457678n 
+
+
+
+//  Refrence (non primitive ) datatypes
+
+//  Array , Object ,  Function
