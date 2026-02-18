b01bd64671c4571e46463b88c0fd1d2fd89831fe
shahzainbhatti190 <shahzainbhatti190@gmail.com>
2026-02-18 10:42:03 +0000

    discuss datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+console.log(2 > 1);                
+console.log(2 >= 1);                    //normal conparisons
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);                    //different data type comparison
+console.log("2" >= 1);
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null <= 0);                  //  not a good practice to compare different data types
+console.log(null == 0);
+
+console.log(undefined > 0 );
+console.log(undefined < 0 );               
+console.log(undefined == 0 );
+console.log(undefined <= 0 );
+
+
+console.log( "2" === 2 );                    //strict check not only check values but also a data types
+
+
+
+
+
---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,13 @@
+// data typers primitive and non primitive
+
+
+// Primitive data type             ye call by value hote hain 
+// there are 7  premitive datatypes
+
+// 1 :  String
+// 2 :  Numbers
+// 3 :  Boolean
+// 4 :  Null
+// 5 :  Undefinied
+// 6 :  Symbol
+// 7 :  BigInt
